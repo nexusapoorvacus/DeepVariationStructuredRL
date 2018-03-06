664fb611c13ea97caad9acdb8fdd6c292595e8ca
nexusapoorvacus <apoorvad@stanford.edu>
2018-03-05 19:07:03 -0800

    using listdir instead

--- create_data_samples.py
+++ create_data_samples.py
@@ -1,19 +1,22 @@
 import json
 import argparse
+import os
 
 IMAGE_DIR = "data/images/"
 OUTPUT_DATA_SAMPLE_FILE = "data/data_samples.json"
 NUM_IMAGES = 10
 
 def create_data_sample_file():
-	image_files = os.scandir(IMAGE_DIR)
 	file_to_write = open(OUTPUT_DATA_SAMPLE_FILE, "w")
 	images = []
-	for im in image_files:
-		images.append(im.name)
+	for im_name in os.listdir(IMAGE_DIR)[:NUM_IMAGES]:
+		images.append(im_name)
 	file_to_write.write(str(images))
 
 def main():
 	print("Creating data sample file for " + str(NUM_IMAGES)  + " images...")
 	create_data_sample_file()
 	print("Done!")
+
+if __name__ == "__main__":
+	main()
