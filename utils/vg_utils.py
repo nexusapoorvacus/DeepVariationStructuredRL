825c790b59a9738dea5cc8cb677e592086a7205a
nexusapoorvacus <apoorvad@stanford.edu>
2018-03-07 03:15:59 -0800

    added ambuguity-UNAWARE mining function

--- utils/vg_utils.py
+++ utils/vg_utils.py
@@ -15,6 +15,18 @@ def predicate_to_aliases(predicate):
 		return PREDICATE_ALIASES[predicate]
 	return (predicate,)
 
+def find_object_neighbors(subject_bbox, image_data_label):
+	image_objects = image_data_label["objects"]
+	neighboring_objects = []
+	x, y, w, h = subject_bbox
+	for obj in image_objects:
+		if subject_bbox != (obj["x"], obj["y"], obj["w"], obj["h"]):
+			if abs(obj["x"] - x) < 0.5 * (obj["w"] + w) and abs(obj["y"] - y) < 0.5 * (obj["h"] + h):
+				neighboring_objects.append(obj)
+	return neighboring_objects
+
+# HELPER FUNCTIONS
+
 def read_aliases(filename):
 	alias_file = open(filename, "r")
 	all_aliases = {}
@@ -32,4 +44,3 @@ def read_aliases(filename):
 PREDICATE_ALIASES = read_aliases(PREDICATE_ALIAS_FILE)
 # load object alias file
 OBJECT_ALIASES = read_aliases(OBJECT_ALIAS_FILE)
-
