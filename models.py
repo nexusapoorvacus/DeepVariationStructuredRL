32a8e3f39af9a269aa437ccdccfa23701d005f70
nexusapoorvacus <apoorvad@stanford.edu>
2018-03-07 19:46:39 -0800

    renamed VGG16 class

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class PretrainedVGG16(nn.Module):
+class VGG16(nn.Module):
 
 	def __init__(self):
 		super(PretrainedVGG16, self).__init__()
