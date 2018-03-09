68f62483f51934bb5c0e18c050927fc484528052
nexusapoorvacus <apoorvad@stanford.edu>
2018-03-08 22:49:40 -0800

    added reset function

--- image_state.py
+++ image_state.py
@@ -1,15 +1,34 @@
+from collections import defaultdict
+
 class ImageState():
 
-	def __init__(self, image_name, gt_scene_graph):
+	def __init__(self, image_name, gt_scene_graph, image_feature, entity_features, entity_proposals, entity_classes, entity_scores):
 		self.image_name = image_name
 		self.gt_scene_graph = gt_scene_graph
 		self.current_scene_graph = {"relationships": [], "entities": []}
+		
 		# number of objects to explore for a subject
 		self.max_objects_to_explore = 5
+		
 		# entities chosen to explore so far
 		self.explored_entities = []
+		
 		# next object to explore
 		self.next_object = None
+		
+		# image feature
+		self.image_feature = image_feature
+		
+		# add object features
+		self.entity_features = entity_features
+		self.entity_proposals = entity_proposals
+		self.entity_classes = entity_classes
+		self.entity_scores = entity_scores
+
+		# previously mined 
+		# per subject for attributes and next object
+		self.previously_mined_attributes = defaultdict(lambda: [])
+		self.previously_mined_next_objects = defaultdict(lambda: [])
 
 		def initialize_entities(self, entity_proposals, entity_classes, entity_scores):
 			pass
@@ -23,4 +42,10 @@ class ImageState():
 			# NOTE: subject_id and object_id are ids for this particular image
 			#	predicate_id is the id from the SAG
 			pass
-
+		
+		def reset(self):
+			self.current_scene_graph = {"relationships": [], "entities":[]}
+			self.explored_entities = []
+			self.next_object = None
+			self.previously_mined_attributes = defaultdict(lambda: [])
+			self.previously_mined_next_objects = defaultdict(lambda: [])
