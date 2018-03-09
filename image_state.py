89ef0684e45449fc257059117e9db1d7f5bc7bea
nexusapoorvacus <apoorvad@stanford.edu>
2018-03-08 19:11:58 -0800

    added method placeholders

--- image_state.py
+++ image_state.py
@@ -3,9 +3,24 @@ class ImageState():
 	def __init__(self, image_name, gt_scene_graph):
 		self.image_name = image_name
 		self.gt_scene_graph = gt_scene_graph
-		self.choose_new_subject = False
 		self.current_scene_graph = {"relationships": [], "entities": []}
+		# number of objects to explore for a subject
+		self.max_objects_to_explore = 5
+		# entities chosen to explore so far
 		self.explored_entities = []
+		# next object to explore
+		self.next_object = None
 
-		def add_entities(self, proposals):
-			pass 
+		def initialize_entities(self, entity_proposals, entity_classes, entity_scores):
+			pass
+	
+		def add_attribute(self, subject_id, attribute_id):
+			# NOTE: subject_id is the id for this particular image
+			#	attribute_id is the id from the SAG
+			pass
+
+		def add_predicate(self, subject_id, predicate_id, object_id):
+			# NOTE: subject_id and object_id are ids for this particular image
+			#	predicate_id is the id from the SAG
+			pass
+
